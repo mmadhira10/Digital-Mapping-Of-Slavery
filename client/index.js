e9d28b94df2bbce9cef71363ec6695e765748376
Mihir Madhira <right2mihir@gmail.com>
2023-03-06 04:05:49 -0500

    setting up app bar

--- client/index.js
+++ client/index.js
@@ -1,7 +1,13 @@
 import React from 'react'
-import  { createRoot }  from 'react-dom/client';
-import App from './src/App.js'
+import ReactDOM from 'react-dom';
+import App from './src/App.js';
+import reportWebVitals from './reportWebVitals';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App/>);
+ReactDOM.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
+reportWebVitals();
--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { ResponsiveAppBar } from ".//components/ResponsiveAppBar";
 
 const App = () =>{
     return (
-        <h1>
-            Hello world! I am using React
-        </h1>
+        <BrowserRouter>
+            <ResponsiveAppBar/>
+        </BrowserRouter>
     )
 }
 
