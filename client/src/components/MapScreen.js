a36c637d75f1ff1f333b63379dae7f980fd66ba9
Mihir Madhira <right2mihir@gmail.com>
2023-03-21 13:58:46 -0400

    map style changes

--- client/src/components/MapScreen.js
+++ client/src/components/MapScreen.js
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import "leaflet/dist/leaflet.css";
+import Box from '@mui/material/Box';
+import paper from './img/paper.png';
 import "./style.css";
 
 import { createTheme, ThemeProvider } from '@mui/material/styles';
@@ -8,11 +10,13 @@ import { MapContainer, TileLayer } from "react-leaflet"
 export default function MapScreen () {
     const theme = createTheme();
     return(
-        <MapContainer center={[48.8566, 2.3522]} xoom={13}>
-            <TileLayer 
-                attributions='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="'https://tile.openstreetmap.org/{z}/{x}/{y}.png'"
-            />
-        </MapContainer>
+        <Box sx={{ flexGrow: 3 }} style={{ backgroundImage: `url(${paper})`, position: 'absolute', left: 0, right: 0, top: 0, height: '100%'}}>
+            <MapContainer center={[48.8566, 2.3522]} xoom={13}>
+                <TileLayer 
+                    attributions='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="'https://tile.openstreetmap.org/{z}/{x}/{y}.png'"
+                />
+            </MapContainer>
+        </Box>
     )
 }
