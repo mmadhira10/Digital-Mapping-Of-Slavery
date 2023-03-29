8a97baf5aa764f433a8cd8ed3856058b6602cc94
Mihir Madhira <right2mihir@gmail.com>
2023-03-29 18:22:26 -0400

    map shows

--- client/src/components/MapScreen.js
+++ client/src/components/MapScreen.js
@@ -11,10 +11,10 @@ export default function MapScreen () {
     const theme = createTheme();
     return(
         <Box sx={{ flexGrow: 3 }} style={{ backgroundImage: `url(${paper})`, position: 'absolute', left: 0, right: 0, top: 0, height: '100%'}}>
-            <MapContainer center={[48.8566, 2.3522]} xoom={13}>
-                <TileLayer 
-                    attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
-                    url="https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}.png?key=R1wykOuwF4jyJO0VreM7"
+            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
             </MapContainer>
         </Box>
